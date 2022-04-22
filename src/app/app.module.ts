48ef33498fa56c7594e456f1fcb164587b010ba5
Varavchik18 <48031083+Varavchik18@users.noreply.github.com>
2022-04-22 10:03:56 +0300

    added tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { TodoComponent } from './todo/todo.component';
+import { NotesComponent } from './notes/notes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    BookmarksComponent,
+    TodoComponent,
+    NotesComponent
   ],
   imports: [
     BrowserModule,
