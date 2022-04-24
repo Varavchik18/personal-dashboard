4a935913172b668262b193cd3dc5740651df9bc5
Varavchik18 <48031083+Varavchik18@users.noreply.github.com>
2022-04-24 13:42:52 +0300

    add/edit note functionality

--- src/app/notes/add-note/add-note.component.ts
+++ src/app/notes/add-note/add-note.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, NgForm, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Note } from 'src/app/shared/note.model';
+import { NoteService } from 'src/app/shared/note.service';
 
 @Component({
   selector: 'app-add-note',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddNoteComponent implements OnInit {
 
-  constructor() { }
+  showValidationErrors: boolean = false;
+
+  constructor(private noteService: NoteService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onFormSubmit(form: NgForm) {
+    console.log(form)
+    if (form.invalid) return this.showValidationErrors = true;
+    else {
+
+      const note = new Note(form.value.title, form.value.content)
+      console.log(note)
+
+      this.noteService.addNote(note);
+      this.router.navigateByUrl("/notes")
+      return this.showValidationErrors = false
+    }
   }
 
 }
---
+++ src/app/notes/edit-note/edit-note.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap, Route, Router } from '@angular/router';
+import { Note } from 'src/app/shared/note.model';
+import { NoteService } from 'src/app/shared/note.service';
+
+@Component({
+  selector: 'app-edit-note',
+  templateUrl: './edit-note.component.html',
+  styleUrls: ['./edit-note.component.scss']
+})
+export class EditNoteComponent implements OnInit {
+  note: Note
+  showValidationErrors: boolean = false;
+
+
+  constructor(private route: ActivatedRoute, private noteService: NoteService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      const idParam = paramMap.get('id')
+      this.note = this.noteService.getNote(idParam)!;
+    })
+  }
+
+  onFormSubmit(form: NgForm) {
+    if (form.invalid) return this.showValidationErrors = true;
+    else {
+      this.noteService.updateNote(this.note.id!, form.value)
+      this.router.navigateByUrl("/notes")
+      return this.showValidationErrors = false
+    }
+  }
+
+  deleteNote() {
+    this.noteService.deleteNote(this.note.id!);
+    this.router.navigateByUrl("/notes")
+  }
+
+}
